f0f445625c9c8fb3573ec5cb43d2025228bbfdfa
Revenger333 <reynaldi525@gmail.com>
2022-04-29 22:12:35 +0700

    grid view dasar dengan presenter, adapter, dan interface nya

--- app/src/main/java/unpar/topcoder/electronicstore_01/presenter/GridPresenter.kt
+++ app/src/main/java/unpar/topcoder/electronicstore_01/presenter/GridPresenter.kt
@@ -1,4 +1,66 @@
 package unpar.topcoder.electronicstore_01.presenter
 
-class GridPresenter {
+import android.util.Log
+import unpar.topcoder.electronicstore_01.model.ProductDetails
+import unpar.topcoder.electronicstore_01.view.GridInterface
+
+class GridPresenter (private var ui:GridInterface){
+    private var prods : ArrayList<ProductDetails> = ArrayList() //array berisi semua produk di database
+    private var currProds : ArrayList<ProductDetails> = ArrayList() // array berisi produk yang mau ditampilkan (akan ditampilkan per 4 prod)
+    //pointer kiri dan kanan untuk prods
+    private var leftCounter=0
+    private var rightCounter=3
+    private var endOfList= false // boolean biar kalau produk sudah ditampilkan semua ga bakal ke update
+
+    //function untuk initial add produk
+    fun loadProducts(prods : ArrayList<ProductDetails>) {
+        this.prods.addAll(prods)
+        for(i in this.leftCounter..this.rightCounter){
+            var tempProduct=prods.get(i)
+            this.currProds.add(tempProduct)
+        }
+
+        this.ui.updateGrid(this.currProds)
+    }
+
+    //function saat tombol add more dipencet
+    fun addProducts(){
+        val sizeProds=this.prods.size
+        //jika masih ada produk yg bisa ditampilin
+        if(rightCounter+4<sizeProds){
+            Log.d("testAddButton","size lebih kecil")
+            this.leftCounter+=4
+            this.rightCounter+=4
+
+            updateCurrProds()
+        }
+        else{
+            Log.d("testAddButton","size lebih besar")
+            //kalau belum semua produk ditampilkan tapi udah diujung banget
+            if(this.endOfList==false){
+                this.leftCounter=this.rightCounter+1
+                this.rightCounter=sizeProds-1
+                this.endOfList=true
+
+                updateCurrProds()
+            }
+        }
+
+    }
+
+    //function nambahin produk
+    fun updateCurrProds(){
+        Log.d("testAddButton",this.leftCounter.toString())
+        Log.d("testAddButton",this.rightCounter.toString())
+        this.currProds.clear() //bersihin currProd
+
+        //masukin 4 barang baru ke currProds sehingga isi currProds jadi 4 produk yg mau ditambah
+        for(i in this.leftCounter..this.rightCounter){
+            var tempProduct=prods.get(i)
+            this.currProds.add(tempProduct)
+        }
+
+        this.ui.updateGrid(this.currProds)
+    }
+
 }
--- app/src/main/java/unpar/topcoder/electronicstore_01/view/GridAdapter.kt
+++ app/src/main/java/unpar/topcoder/electronicstore_01/view/GridAdapter.kt
@@ -1,4 +1,71 @@
 package unpar.topcoder.electronicstore_01.view
 
-class GridAdapter {
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.fragment.app.Fragment
+import unpar.topcoder.electronicstore_01.R
+import unpar.topcoder.electronicstore_01.databinding.ProductGridEntryBinding
+import unpar.topcoder.electronicstore_01.model.ProductDetails
+import unpar.topcoder.electronicstore_01.presenter.GridPresenter
+
+class GridAdapter (private var activity: Activity, private var presenter: GridPresenter): BaseAdapter(), View.OnClickListener{
+    private lateinit var gridItemBinding: ProductGridEntryBinding
+
+
+    private var prods: MutableList<ProductDetails> = ArrayList()
+
+
+
+    override fun getCount(): Int {
+        return this.prods.size
+    }
+
+    override fun getItem(position: Int): ProductDetails {
+        return this.prods[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView: View?=convertView
+        if(convertView==null){
+            this.gridItemBinding = ProductGridEntryBinding.inflate(this.activity.layoutInflater)
+            convertView = this.gridItemBinding.root
+            convertView.setTag(this.gridItemBinding)
+        }
+        else{
+            this.gridItemBinding= convertView.tag as ProductGridEntryBinding
+        }
+
+        val currentProduct = this.getItem(position)
+        this.updateLayout(currentProduct)
+        return this.gridItemBinding.root
+    }
+
+    //function update layout (buat recycle)
+    fun updateLayout(currentProduct : ProductDetails){
+        this.gridItemBinding.productName.text=currentProduct.getNama()
+        this.gridItemBinding.productCategory.text=currentProduct.getKategori()
+        this.gridItemBinding.productCondition.text = ""+currentProduct.getKondisi()+"%"
+        this.gridItemBinding.productPrice.text = ""+currentProduct.getHarga()
+        this.gridItemBinding.productImage.setImageResource(currentProduct.getImageSource())
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+    //function untuk update arraylist yang ada di adapter
+    fun update(prods: ArrayList<ProductDetails>){
+        this.prods.addAll(prods)
+        this.notifyDataSetChanged()
+    }
+
 }
