0b617ea44ba45d148ee6b2f66f8e2487956b3c68
Revenger333 <reynaldi525@gmail.com>
2022-05-19 10:56:35 +0700

    menambahkan produk

--- app/src/main/java/unpar/topcoder/electronicstore_01/model/AllProducts.kt
+++ app/src/main/java/unpar/topcoder/electronicstore_01/model/AllProducts.kt
@@ -4,13 +4,37 @@ import unpar.topcoder.electronicstore_01.R
 
 object  AllProducts {
     val products = arrayOf<ProductDetails>(
+        ProductDetails("Galaxy Note", ProductCode.PNEW, ProductCode.SMARTPHONE, "4.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 2", ProductCode.PNEW, ProductCode.SMARTPHONE, "5.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 3", ProductCode.PNEW, ProductCode.SMARTPHONE, "6.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 4", ProductCode.PNEW, ProductCode.SMARTPHONE, "7.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 7. 6.1 Galaxy Note Fan Edition", ProductCode.PNEW, ProductCode.SMARTPHONE, "8.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 8", ProductCode.PNEW, ProductCode.SMARTPHONE, "9.200.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 9", ProductCode.PNEW, ProductCode.SMARTPHONE, "10.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 10", ProductCode.PNEW, ProductCode.SMARTPHONE, "11.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 10+", ProductCode.PNEW, ProductCode.SMARTPHONE, "12.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 20", ProductCode.PNEW, ProductCode.SMARTPHONE, "13.500.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Note 20+", ProductCode.PNEW, ProductCode.SMARTPHONE, "9.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
         ProductDetails("Galaxy Note20 Ultra 5G", ProductCode.PNEW, ProductCode.SMARTPHONE, "18.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
         ProductDetails("Galaxy A53 5G", ProductCode.PMEDIUM, ProductCode.SMARTPHONE, "5.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
-        ProductDetails("Galaxy Tab S8+ 5G", ProductCode.PHIGH, ProductCode.TABLET, "16.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+
+        ProductDetails("Galaxy Tab 7.0", ProductCode.PHIGH, ProductCode.TABLET, "7.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Tab 4 7.0", ProductCode.PHIGH, ProductCode.TABLET, "8.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Tab Pro 10.1", ProductCode.PHIGH, ProductCode.TABLET, "8.699.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Tab Pro 12.2", ProductCode.PHIGH, ProductCode.TABLET, "9.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy TabPro S", ProductCode.PHIGH, ProductCode.TABLET, "10.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
         ProductDetails("Galaxy Tab S8+ (Wi-Fi)", ProductCode.PMEDIUM, ProductCode.TABLET, "9.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
-        ProductDetails("Galaxy Watch4 LTE (40mm)", ProductCode.PLOW, ProductCode.WATCHES, "3.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
-        ProductDetails("Galaxy Watch Active2", ProductCode.PHIGH, ProductCode.WATCHES, "4.399.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
-        ProductDetails("Galaxy Buds2", ProductCode.PMEDIUM, ProductCode.GALAXYBUDS, "1.699.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3)
+
+        ProductDetails("Samsung Galaxy Watch", ProductCode.PMEDIUM, ProductCode.WATCHES, "9.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Samsung Galaxy Watch Active", ProductCode.PMEDIUM, ProductCode.WATCHES, "10.999.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Watch4 LTE (40mm)", ProductCode.PLOW, ProductCode.WATCHES, "11.500.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Watch Active 2", ProductCode.PHIGH, ProductCode.WATCHES, "10.399.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+
+        ProductDetails("Galaxy Buds", ProductCode.PMEDIUM, ProductCode.GALAXYBUDS, "1.699.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Buds Plus", ProductCode.PMEDIUM, ProductCode.GALAXYBUDS, "2.500.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Buds Live", ProductCode.PMEDIUM, ProductCode.GALAXYBUDS, "2.899.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Buds Pro", ProductCode.PMEDIUM, ProductCode.GALAXYBUDS, "3.000.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3),
+        ProductDetails("Galaxy Buds2", ProductCode.PMEDIUM, ProductCode.GALAXYBUDS, "3.500.000", R.drawable.test_image, R.drawable.test_image2, R.drawable.test_image3)
     )
 }
 
