d2d0d9fba388be6a7926e9e3ea2f644d05a79cfd
Nadia Clarissa Hermawan <6181901013@student.unpar.ac.id>
2022-05-21 20:27:21 +0700

    category & name filter, sorting OK

--- app/src/main/java/unpar/topcoder/electronicstore_01/presenter/ListPresenter.kt
+++ app/src/main/java/unpar/topcoder/electronicstore_01/presenter/ListPresenter.kt
@@ -1,35 +1,120 @@
 package unpar.topcoder.electronicstore_01.presenter
 
+import android.util.Log
 import unpar.topcoder.electronicstore_01.model.AllProducts
+import unpar.topcoder.electronicstore_01.model.ProductCode
 import unpar.topcoder.electronicstore_01.model.ProductDetails
 import unpar.topcoder.electronicstore_01.view.IList
 
-class ListPresenter(private val ui : IList) {
+class ListPresenter (private val ui : IList) {
     private var products : ArrayList<ProductDetails> = ArrayList()
 
     //load & update produk u/ disalurkan ke UI (fragment)
-    fun updateList(offset : Int) {
+    fun updateList (offset : Int) {
+        Log.e("DEBUG", "offset: "+offset )
         var dataOffset = offset
         val prods : ArrayList<ProductDetails> = ArrayList()
-        if(dataOffset < AllProducts.products.size) {
-            for(i in dataOffset..(dataOffset + 4)) {
-                if(i < AllProducts.products.size) {
-                    if(i == dataOffset + 4){
+        if (dataOffset < AllProducts.products.size && offset != -1) {
+            for (i in dataOffset..(dataOffset + 4)) {
+                if (i < AllProducts.products.size) {
+                    if (i == dataOffset + 4){
                         dataOffset = i + 1
                     }
                     prods.add(AllProducts.products[i])
-                }else {
+                } else {
                     dataOffset = i
                     break
                 }
             }
             this.products.addAll(prods)
             this.ui.updateList(this.products, dataOffset)
+        } else if (offset == - 1) {
+            this.ui.updateList(this.products, dataOffset)
         }
     }
 
     //send current product details to list Fragment -> DetailFragment
-    fun goDetailsPage(currentProd : ProductDetails){
+    fun goDetailsPage (currentProd : ProductDetails){
         this.ui.moveToDetailsPage(currentProd)
+    }
+
+    //change category
+    fun changeCategory (selectedCategory : Int, dataOffset : Int) {
+        //all
+        if (selectedCategory == -1) {
+            this.updateList(-1)
+        } else {
+            val result = ArrayList<ProductDetails>()
+            val size = AllProducts.products.size
+            var selection: String = ""
+
+            // Smartphone
+            if (selectedCategory == 0) {
+                selection = ProductCode.SMARTPHONE
+            // Tablet
+            } else if (selectedCategory == 1) {
+                selection = ProductCode.TABLET
+            // Watches
+            } else if (selectedCategory == 2) {
+                selection = ProductCode.WATCHES
+            // Galaxy buds
+            } else if (selectedCategory == 3) {
+                selection = ProductCode.GALAXYBUDS
+            }
+
+            //loop ambil data sesuai kategori
+            for (i in 0..(size - 1)) {
+                if (AllProducts.products[i].getKategori() == selection) {
+                    result.add(AllProducts.products[i])
+                }
+            }
+            this.ui.updateList(result, dataOffset)
+        }
+    }
+
+    //filter nama produk
+    fun search (keyword : String) {
+        val result = ArrayList<ProductDetails>()
+        val size = AllProducts.products.size
+
+        for (i in 0..(size - 1)) {
+            if (AllProducts.products[i].getNama().contains(keyword, ignoreCase = true)) {
+                result.add(AllProducts.products[i])
+            }
+        }
+        this.ui.updateList(result, -1)
+    }
+
+    //sorting
+    fun sorting (attr : Int, mode : Int) {
+        //by name
+        if (attr == 0) {
+            if (mode == 0) {
+                this.processSortingResult(this.products.sortedBy { it.getNama() })
+            } else {
+                this.processSortingResult(this.products.sortedByDescending { it.getNama() })
+            }
+        //by price
+        } else if (attr == 1) {
+            if (mode == 0) {
+                this.processSortingResult(this.products.sortedBy { it.getHarga() })
+            } else {
+                this.processSortingResult(this.products.sortedByDescending { it.getHarga() })
+            }
+        //by conditions
+        } else {
+            if (mode == 0) {
+                this.processSortingResult(this.products.sortedBy { it.getKondisi() })
+            } else {
+                this.processSortingResult(this.products.sortedByDescending { it.getKondisi() })
+            }
+        }
+    }
+
+    //process sort result
+    fun processSortingResult (result : List<ProductDetails>) {
+        this.products.clear()
+        this.products.addAll(result)
+        this.ui.updateList(this.products, -1)
     }
 }
--- app/src/main/java/unpar/topcoder/electronicstore_01/view/ListFragment.kt
+++ app/src/main/java/unpar/topcoder/electronicstore_01/view/ListFragment.kt
@@ -1,22 +1,31 @@
 package unpar.topcoder.electronicstore_01.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import org.parceler.Parcels
+import unpar.topcoder.electronicstore_01.R
 import unpar.topcoder.electronicstore_01.databinding.ProductListFragmentBinding
 import unpar.topcoder.electronicstore_01.model.Page
 import unpar.topcoder.electronicstore_01.model.ProductDetails
 import unpar.topcoder.electronicstore_01.presenter.ListPresenter
 
 //fragment
-class ListFragment : Fragment(), IList,View.OnClickListener {
+class ListFragment : Fragment(), IList, View.OnClickListener, AdapterView.OnItemSelectedListener {
     private lateinit var listBinding : ProductListFragmentBinding
     private lateinit var presenter : ListPresenter
     private lateinit var adapter : ListAdapter
     private var dataOffset : Int = 0
+    private var sortByName : Int = 0
+    private var sortByPrice : Int = 0
+    private var sortByCondition : Int = 0
 
     //constructor
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
@@ -28,10 +37,18 @@ class ListFragment : Fragment(), IList,View.OnClickListener {
         this.adapter = ListAdapter(requireActivity(), this.presenter)
         this.listBinding.lstProducts.adapter = this.adapter
 
+        //setup change category spinner
+        this.setupSpinner()
+
+        //setup text filter listener
+        this.setupFilter()
+
         //set click listener
         this.listBinding.layoutTypeGrid.setOnClickListener(this::onClick)
-        this.listBinding.layoutTypeList.setOnClickListener(this::onClick)
         this.listBinding.buttonLoadMore.setOnClickListener(this::onClick)
+        this.listBinding.productName.setOnClickListener(this::onClick)
+        this.listBinding.productPrice.setOnClickListener(this::onClick)
+        this.listBinding.productCondition.setOnClickListener(this::onClick)
 
         //initial products
         this.callUpdateList()
@@ -43,16 +60,36 @@ class ListFragment : Fragment(), IList,View.OnClickListener {
     companion object {
         fun getInstance () : ListFragment {
             val instance = ListFragment()
-            //todo : initialize instance's attributes if needed
-            //instance.attr_name = bablabla..
             return instance
         }
+    }
+
+    //setup spinner kategori
+    fun setupSpinner() {
+        val spinnr = this.listBinding.dropdownCategory
+        val spinnerValues = arrayOf<String>("All", "Smartphone", "Tablet", "Watches", "Galaxy Buds")
+        val adapter: ArrayAdapter<*> = ArrayAdapter<Any>(requireContext(), R.layout.spinner_layout, spinnerValues)
+        adapter.setDropDownViewResource(R.layout.spinner_layout)
+        spinnr.adapter = adapter
+        spinnr.onItemSelectedListener = this
+    }
+
+    //setup text filter listener
+    fun setupFilter() {
+        this.listBinding.searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                val keyword = listBinding.searchBar.text.toString()
+                presenter.search(keyword)
+            }
+        })
     }
 
     //onclick listener method
     override fun onClick(view: View?) {
         //ganti ke grid frag
-        if(view == this.listBinding.layoutTypeGrid){
+        if (view == this.listBinding.layoutTypeGrid) {
             var pg = Bundle()
             pg.putInt(Page.PAGE, Page.GRID_PAGE)
             parentFragmentManager.setFragmentResult(Page.CHANGE_PAGE_LISTENER, pg)
@@ -60,13 +97,40 @@ class ListFragment : Fragment(), IList,View.OnClickListener {
             var target = Bundle()
             target.putInt("target", this.dataOffset)
             parentFragmentManager.setFragmentResult(Page.SYNC_LISTENER, target)
-        //load more
-        }
-        else if(view == this.listBinding.layoutTypeList){
 
-        }
-        else if(view == this.listBinding.buttonLoadMore) {
+        //load more
+        } else if (view == this.listBinding.buttonLoadMore) {
             this.callUpdateList()
+
+        //sort product's name
+        } else if (view == this.listBinding.productName) {
+            if (this.sortByName == 0) {
+                this.presenter.sorting(0, 0)
+                this.sortByName = 1
+            } else {
+                this.presenter.sorting(0, 1)
+                this.sortByName = 0
+            }
+
+        //sort product's price
+        } else if (view == this.listBinding.productPrice) {
+            if (this.sortByPrice == 0) {
+                this.presenter.sorting(1, 0)
+                this.sortByPrice = 1
+            } else {
+                this.presenter.sorting(1, 1)
+                this.sortByPrice = 0
+            }
+
+        //sort product's condition
+        } else if (view == this.listBinding.productCondition) {
+            if (this.sortByCondition == 0) {
+                this.presenter.sorting(2, 0)
+                this.sortByCondition = 1
+            } else {
+                this.presenter.sorting(2, 1)
+                this.sortByCondition = 0
+            }
         }
     }
 
@@ -78,7 +142,9 @@ class ListFragment : Fragment(), IList,View.OnClickListener {
     //salurkan updated product list dari presenter ke adapter untuk ditampilkan
     override fun updateList(products: ArrayList<ProductDetails>, newDataOffset : Int) {
         this.adapter.updateList(products)
-        this.dataOffset = newDataOffset
+        if (newDataOffset != -1) {
+            this.dataOffset = newDataOffset
+        }
     }
 
     //move to detailsFragment
@@ -94,4 +160,12 @@ class ListFragment : Fragment(), IList,View.OnClickListener {
         pg.putInt(Page.PAGE, Page.DETAIL_PAGE)
         parentFragmentManager.setFragmentResult(Page.CHANGE_PAGE_LISTENER, pg)
     }
+
+    //select listener untuk spinner
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        this.presenter.changeCategory(p2-1, this.dataOffset)
+    }
+
+    //do nothing
+    override fun onNothingSelected(p0: AdapterView<*>?) { }
 }
